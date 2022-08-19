c2aae23cbde1df2ea34cfdb36610616db28d8436
tara <tara_m_oliver@yahoo.com>
2022-08-19 01:17:34 -0400

    added iconbar

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -216,11 +216,39 @@ var IconBar = function IconBar() {
     className: "iconbar"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h2", {
     className: "header"
-  }, "Shop shoes that are meant for you."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("ul", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.Link, {
+  }, "Shop shoes that are meant for you."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("ul", {
+    className: "bubble-container"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("li", {
+    className: "bubble-item"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.Link, {
     to: "/Sneakers"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
     className: "imgContainer"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Sneakers")))))));
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", {
+    src: '/Sneaker.png',
+    alt: "sneaker_bubble",
+    className: "bubble-img"
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Sneakers")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("li", {
+    className: "bubble-item"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.Link, {
+    to: "/Sandals"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "imgContainer"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", {
+    src: '/Sandals.png',
+    alt: "sandal_bubble",
+    className: "bubble-img"
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Sandals")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("li", {
+    className: "bubble-item"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.Link, {
+    to: "/Accessories"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "imgContainer"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", {
+    src: '/Accessories.png',
+    alt: "accessories_bubble",
+    className: "bubble-img"
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Accessories")))))));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IconBar);
@@ -332,11 +360,15 @@ var NavBar = function NavBar(_ref) {
       return openModal('login');
     }
   }, "Sign in");
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("header", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
     className: "nav-bar"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("header", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", {
     className: "logo"
-  }, "Shoesy"), session_button));
+  }, "Shoesy"), session_button, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("path", {
+    d: "M9 22a2 2 0 100-4 2 2 0 000 4zm7 0a2 2 0 100-4 2 2 0 000 4zm5-17H5.665l-.687-3.21A1 1 0 004 1H1a1 1 0 000 2h2.191l2.831 13.21a.962.962 0 00.064.159c.017.053.038.105.063.155a.979.979 0 00.133.153.926.926 0 00.088.1c.058.041.12.077.185.105.034.022.07.042.107.06A.993.993 0 007 17h11a1 1 0 00.958-.713l3-10A1.001 1.001 0 0021 5zm-2.244 5H16V7h3.656l-.9 3zM7.819 15l-.6-3H9v3H7.819zM11 12h3v3h-3v-3zm0-2V7h3v3h-3zM9 7v3H6.82l-.6-3H9zm8.256 8H16v-3h2.156l-.9 3z"
+  }))));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NavBar);
--- frontend/components/icon_bar/icon_bar_container.jsx
+++ frontend/components/icon_bar/icon_bar_container.jsx
@@ -7,15 +7,45 @@ const IconBar = () => {
         <div className='iconbar'>
             <h2 className='header'>Shop shoes that are meant for you.</h2>
             <div>
-                <ul>
-                    <li>
+                <ul className='bubble-container'>
+                    <li className='bubble-item'>
                         <Link to='/Sneakers'>
                             <div className='imgContainer'>
-                                {/* <img src={require('./images/Sneaker.png').default}/> */}
-                                {/* <img src={require('./logo.jpeg')} /> */}
+                                <img src={'/Sneaker.png'}
+                                     alt="sneaker_bubble"
+                                     className='bubble-img'
+                                />
                             </div>
                             <div>
                                 <p>Sneakers</p>
+                            </div>
+                        </Link>
+                    </li>
+
+                    <li className='bubble-item'>
+                        <Link to='/Sandals'>
+                            <div className='imgContainer'>
+                                <img src={'/Sandals.png'}
+                                    alt="sandal_bubble"
+                                    className='bubble-img'
+                                />
+                            </div>
+                            <div>
+                                <p>Sandals</p>
+                            </div>
+                        </Link>
+                    </li>
+
+                    <li className='bubble-item'>
+                        <Link to='/Accessories'>
+                            <div className='imgContainer'>
+                                <img src={'/Accessories.png'}
+                                    alt="accessories_bubble"
+                                    className='bubble-img'
+                                />
+                            </div>
+                            <div>
+                                <p>Accessories</p>
                             </div>
                         </Link>
                     </li>
